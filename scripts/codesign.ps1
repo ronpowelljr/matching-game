4289a9c7e803d3b00613717f17a08954ac7a2d04
Dominic Motuka <dominic.motuka@gmail.com>
2019-11-14 15:28:32 +0300

    Correctly specify path name
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- scripts/codesign.ps1
+++ scripts/codesign.ps1
@@ -5,7 +5,7 @@ Set-LocalUser -Name "circleci" -Password $password
 $username = "circleci"
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
 Start-Job -Name build -Credential $cred -ScriptBlock { 
-   Set-Location C:\Users\circleci\project\MatchingGame\bin\Release\netcoreapp3.0\win-x64\publish\MatchingGame.exe
+   Set-Location C:\Users\circleci\project\MatchingGame\bin\Release\netcoreapp3.0\win-x64\publish\
    npm run build:dev
 }
 Receive-Job -Wait -Name build
