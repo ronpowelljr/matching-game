35d2a624791e796e78739956fc8aaffba368dde1
Dominic Motuka <dominic.motuka@gmail.com>
2019-11-15 14:52:22 +0300

    Reset the system password to then create a priviledged session
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- scripts/PasswordResetCredsCert.ps1
+++ scripts/PasswordResetCredsCert.ps1
@@ -4,8 +4,6 @@ $password = ConvertTo-SecureString $raw_password -AsPlainText -Force
 Set-LocalUser -Name "circleci" -Password $password
 $username = "circleci"
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
-Start-Job -Name build -Credential $cred -ScriptBlock { 
-   Set-Location C:\Users\circleci\project\MatchingGame\bin\Release\netcoreapp3.0\win-x64\publish\
-   npm run build:dev
+Start-Job -Credential $cred -ScriptBlock { # Certificate publisher
+  #use Import-PfxCertificate here
 }
-Receive-Job -Wait -Name build
