4701a5ce44c70f5c31f3750fc36fc051186aa251
Dominic Motuka <dominic.motuka@gmail.com>
2019-11-15 15:51:27 +0300

    Use Docker to codesign app
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

---
+++ scripts/RunDockerMakeCert.ps1
@@ -0,0 +1,3 @@
+#mount the app you want to sign instead of C:\testapp
+docker run -v "C:\Users\circleci\project\MatchingGame\:C:\testapp" -it  mcr.microsoft.com
+/windows/servercore:ltsc2019 powershell.exe -executionpolicy bypass C:\testapp\scripts\makecert.ps1
--- scripts/makecert.ps1
+++ scripts/makecert.ps1
@@ -1,18 +1,23 @@
 # Set up a self-signed certificate to be able to code sign on CircleCI on Windows
 
 $params = @{
-  Subject = "CN=CircleCI Test"
+  Subject = "CN=CircleCI Test With Root"
   KeyUsage = "DigitalSignature"
   KeyAlgorithm = 'RSA'
   KeySpec           = "Signature"
-  FriendlyName =  "CircleCI Test"
-  CertStoreLocation = "Cert:\LocalMachine\My"
+  FriendlyName =  "CircleCI Test3"
+  CertStoreLocation = "Cert:\CurrentUser\My" # Note that this method lets you create a private key in CurrentUser\My
   KeyExportPolicy = 'Exportable'
   Type = "Custom"
   TextExtension=@("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
 }
 
 $cert = New-SelfSignedCertificate @params
-Export-Certificate -Cert $cert -File C:\ucert.sst -Type SST
-Import-Certificate -File C:\ucert.sst -CertStoreLocation Cert:\LocalMachine\Root
-Import-Certificate -File C:\ucert.sst -CertStoreLocation Cert:\CurrentUser\My
+Export-Certificate -Cert $cert -File C:\ucert.cer
+Import-Certificate -File C:\ucert.cer -CertStoreLocation Cert:\LocalMachine\Root
+
+Get-ChildItem cert:\CurrentUser\My -CodeSigning
+
+$root_cert | Select-Object *
+$cert| Select-Object *
+Set-AuthenticodeSignature -FilePath C:\testapp\bin\Release\netcoreapp3.0\win-x64\publish\MatchingGame.exe  -Certificate $cert | Select-Object *
