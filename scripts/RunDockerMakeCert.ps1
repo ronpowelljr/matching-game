6a4a02f773b1809d287eec4624f360c6e87aa590
Ron Powell <ron.powell.jr@gmail.com>
2019-12-19 12:31:48 -0800

    Delete RunDockerMakeCert.ps1

--- scripts/RunDockerMakeCert.ps1
+++
@@ -1,2 +0,0 @@
-#mount the app you want to sign instead of C:\testapp
-docker run -v "C:\Users\circleci\project\MatchingGame\:C:\testapp"  mcr.microsoft.com/windows/servercore:ltsc2019 powershell.exe -executionpolicy bypass C:\testapp\scripts\makecert.ps1
