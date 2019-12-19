9aec677a7f99198ec3490f625f8cce2bcf15d167
Ron Powell <ron.powell.jr@gmail.com>
2019-12-19 12:46:01 -0800

    Delete PasswordResetCredsCert.ps1

--- scripts/PasswordResetCredsCert.ps1
+++
@@ -1,9 +0,0 @@
-Add-Type -AssemblyName System.web
-$raw_password = [System.Web.Security.Membership]::GeneratePassword(42, 10)
-$password = ConvertTo-SecureString $raw_password -AsPlainText -Force
-Set-LocalUser -Name "circleci" -Password $password
-$username = "circleci"
-$cred = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
-Start-Job -Credential $cred -ScriptBlock { # Certificate publisher
-  #use Import-PfxCertificate here
-}
